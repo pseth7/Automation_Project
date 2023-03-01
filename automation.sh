afca045d925266c5aa1d82ee8b1bf8fd216c00dc
pseth7 <124786228+pseth7@users.noreply.github.com>
2023-03-01 21:09:18 +0530

    Add files via upload

--- automation.sh
+++ automation.sh
@@ -1,18 +1,35 @@
+cd /var/www/html/
+
+if [ -f inventory.html ]; then
+
+echo "inventory.html exists "
+else
+
+echo -e "Log Type\t\tDate Created \t\t Type \t\t Size " > /var/www/html/inventory.html
+
+fi
+
 sudo apt update -y
 apache_present=$(dpkg -l apache2|grep ii|wc -l)
 
-if [ $apache_present -eq 0 ]
+if  [ $apache_present -eq 0 ]
 then
 sudo apt install apache2
 fi
 
-systemctl enable apache2
 sudo systemctl start apache2
 timestamp=$(date '+%d%m%Y-%H%M%S')
-
-tar -cvf /tmp/Priyesh-httpd-logs-$timestamp.tar /var/log/apache2/*.log
-
 myname="Priyesh"
-
 s3_bucket="upgrad-priyesh"
+
+tar -cvf /tmp/$myname-httpd-logs-$timestamp.tar /var/log/apache2/*.log
+size=$(du -sh /tmp/$myname-httpd-logs-$timestamp.tar |awk '{print $1}')
+echo -e "httpd-logs\t\t$timestamp \t\t tar \t\t $size" >>/var/www/html/inventory.html
+
+
+
 aws s3 \cp /tmp/${myname}-httpd-logs-${timestamp}.tar \s3://${s3_bucket}/${myname}-httpd-logs-${timestamp}.tar
+cd /etc/cron.d/
+if [ ! -f automation.sh ];then
+echo "5 4 * * * root /root/Automation_Project/automation.sh" >/etc/cron.d/automation
+fi
